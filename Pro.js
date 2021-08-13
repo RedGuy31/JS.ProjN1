57584443f9d155f1b467d13b308566533d13d21f
RedGuy <I.maisuradze31.19@gmail.com>
2021-08-13 16:14:56 +0400

    Smlogadded

--- Pro.js
+++ Pro.js
@@ -1,6 +1,6 @@
 let secretNumb = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
+let highscore = 0;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
@@ -14,6 +14,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumb;
     document.querySelector("body").style.backgroundColor = "#60b346";
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   }
   // Too Hight!
   else if (guess > secretNumb) {
