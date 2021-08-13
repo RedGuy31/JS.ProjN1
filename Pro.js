e91d6f2df2cf396f516cbbc45a6783a49ec6abcc
RedGuy <I.maisuradze31.19@gmail.com>
2021-08-13 15:35:16 +0400

    gl

--- Pro.js
+++ Pro.js
@@ -1,18 +1,22 @@
 const secretNumb = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = secretNumb;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
-
+  // No input!
   if (!guess) {
     document.querySelector(".message").textContent = "🤔 No Number !";
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else if (guess === secretNumb) {
+  }
+  // Win !
+  else if (guess === secretNumb) {
     document.querySelector(".message").textContent = "😄 Correct Number!";
-  } else if (guess > secretNumb) {
+    document.querySelector(".number").textContent = secretNumb;
+    document.querySelector("body").style.backgroundColor = "#60b346";
+    document.querySelector(".number").style.width = "30rem";
+  }
+  // Too Hight!
+  else if (guess > secretNumb) {
     if (score > 1) {
       document.querySelector(".message").textContent = "😥 Too hight!";
       score--;
@@ -21,6 +25,7 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "🥺 You lost this game";
       document.querySelector(".score").textContent = 0;
     }
+    // Too Low!
   } else if (guess < secretNumb) {
     if (score > 1) {
       document.querySelector(".message").textContent = "😎 Too Low!";
