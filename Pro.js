49a40e4758ca647026536c01ae518359d2f228d5
RedGuy <I.maisuradze31.19@gmail.com>
2021-08-13 14:51:31 +0400

    logics

--- Pro.js
+++ Pro.js
@@ -1,1 +1,13 @@
-document.querySelector(".message");
+const secretNumb = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumb;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "🤔 No Number !";
+  } else if (guess === secretNumb) {
+    document.querySelector(".message").textContent = "😄 Correct Number!";
+  }
+});
